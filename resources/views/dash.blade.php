246f38c8e5f0d6b5f9d51c61d5d37d9c47da9925
robbynoviantoo <heyitsrobby99@gmail.com>
2024-07-29 14:25:25 +0700

    oke

--- resources/views/dash.blade.php
+++ resources/views/dash.blade.php
@@ -7,69 +7,73 @@
     <title>Dashboard Defect</title>
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
     <link rel="stylesheet" href="public/css/style.css">
-    <style>
-        .defect-container {
-            display: flex;
-            flex-wrap: wrap;
-            justify-content: center; /* Menyelaraskan konten ke tengah */
-        }
-        .defect-item {
-            width: 200px;
-            margin: 10px;
-            text-align: center;
-        }
-        .defect-image {
-            max-width: 100%;
-            height: auto;
-        }
-        .defect-description {
-            margin-top: 5px;
-            font-size: 0.9em;
-            color: #555;
-            text-align: center; /* Menyelaraskan teks ke tengah */
-        }
-    </style>
+    <script>
+        // Auto refresh setiap 5 detik
+        setTimeout(function(){
+            location.reload();
+        }, 5000);
+    </script>
 </head>
-<body style="height: 100vh; overflow-y: auto;">
-    @foreach($defects as $defect)
-    <div class="judul">
-        <div class="subjudul">
-            <h1>Daily End Of Line Validation</h1>
-        </div>
-    </div>
-    <div class="bagi2" style="height: calc(100vh - 100px); overflow-y: auto;">
-        <div class="kiri">
-            <div class="content">
-                <h1>Time & Place</h1>
-                <div>Tanggal: {{ $defect->tanggal }}</div>
-                <div>Cell: {{ $defect->cell }}</div>
-            </div>
-            <div class="content">
-                <h1>QTY Status</h1>
-                <div>QTY OK: {{ $defect->qtyok }}</div>
-                <div>QTY Not OK: {{ $defect->qtynok }}</div>
+<body>
+    @if($defects->isNotEmpty())
+        @php
+            $defect = $defects->last(); // Mengambil inputan terakhir
+            $images = json_decode($defect->images);
+        @endphp
+        <div class="judul">
+            <div class="subjudul">
+                <h1>Daily End Of Line Validation</h1>
             </div>
         </div>
-        <div class="kanan">
-            @if($defect->images)
-                @php
-                    $images = json_decode($defect->images);
-                @endphp
-                <div class="defect-container">
-                    @foreach($images as $index => $image)
-                    <div class="defect-item">
-                        <img src="{{ asset('storage/app/public/' . $image) }}" alt="Defect Image" class="defect-image">
-                        <div class="defect-description">
-                            {{ explode(';', $defect->defect)[$index] ?? 'Keterangan tidak tersedia' }}
-                        </div>
-                    </div>
-                    @endforeach
+        <div class="bagi2" style="height: calc(100vh - 100px); overflow-y: auto;">
+            <div class="kiri">
+                <div class="content">
+                    <h1>Time & Place</h1>
+                    <table class="table">
+                        <tr>
+                            <td>Tanggal</td>
+                            <td>: {{ $defect->tanggal }}</td>
+                        </tr>
+                        <tr>
+                            <td>Cell</td>
+                            <td>: {{ $defect->cell }}</td>
+                        </tr>
+                    </table>
                 </div>
-            @else
-                <div>Tidak ada gambar.</div>
-            @endif
+                <div class="content">
+                    <h1>QTY CHECK</h1>
+                    <table class="table">
+                        <tr>
+                            <td>OK</td>
+                            <td>: {{ $defect->qtyok }}</td> <!-- Menjaga sejajar -->
+                        </tr>
+                        <tr>
+                            <td>Not OK</td>
+                            <td>: {{ $defect->qtynok }}</td> <!-- Menjaga sejajar -->
+                        </tr>
+                    </table>
+                </div>
+            </div>
+            <div class="kanan">
+                <h1 class="judul" style="margin-left: 70px;">Detail Defect</h1>
+                @if($defect->images)
+                    <div class="defect-container">
+                        @foreach($images as $index => $image)
+                        <div class="defect-item">
+                            <img src="{{ asset('storage/app/public/' . $image) }}" alt="Defect Image" class="defect-image">
+                            <div class="defect-description">
+                                {{ explode(';', $defect->defect)[$index] ?? 'Keterangan tidak tersedia' }}
+                            </div>
+                        </div>
+                        @endforeach
+                    </div>
+                @else
+                    <div>Tidak ada gambar.</div>
+                @endif
+            </div>
         </div>
-    </div>
-    @endforeach
+    @else
+        <div>Tidak ada defect yang tersedia.</div>
+    @endif
 </body>
 </html>
